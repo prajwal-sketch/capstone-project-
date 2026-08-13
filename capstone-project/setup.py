5b73d881a62ca77d8da724eafa69b5c48ce1fc6e
prajwal-sketch <shindeprajwal237@gmail.com>
2026-08-13 11:26:59 +0530

    cokkiecutter

---
+++ capstone-project/setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='this project is around movie sentiment analysis to improve and mlops understanding around mlops concept',
+    author='Prajwal',
+    license='',
+)
